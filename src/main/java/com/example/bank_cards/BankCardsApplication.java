b44ecbf74b5eb497a32fad98972ce4f3bb1b1bcd
Egor Medvedev <egor-medvedev00@mail.ru>
2025-05-09 23:08:22 +0300

    application class

--- src/main/java/com/example/bank_cards/BankCardsApplication.java
+++ src/main/java/com/example/bank_cards/BankCardsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankCardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankCardsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankCardsApplication.class, args);
+    }
 
 }
